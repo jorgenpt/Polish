e3d7f6f322f4662f52cd547e71df354cfeda2754
Jørgen P. Tjernø <jorgenpt@gmail.com>
2010-07-18 00:13:07 -0700

    Slightly sturdier comparison of version numbers in clientside.

--- html/polish.js
+++ html/polish.js
@@ -1,9 +1,29 @@
-function getQueryVariable(variable) { 
+function versionGreaterThan(version1, version2)
+{
+    var versionLength = Math.min(version1.length, version2.length);
+    for (var i = 0; i < versionLength)
+    {
+        var v1 = parseInt(version1[i]);
+        var v2 = parseInt(version2[i]);
+
+        if (v1 > v2)
+            return true;
+        if (v1 < v2)
+            return false;
+    }
+
+    if (version1.length > version2.length)
+        return true;
+    return false;
+}
+function getQueryVariable(variable) 
+
     var query = window.location.search.substring(1); 
     var vars = query.split("&"); 
-    for (var i=0;i<vars.length;i++) { 
+    for (var i = 0; i < vars.length; i++) { 
         var pair = vars[i].split("="); 
-        if (pair[0] == variable) { 
+        if (pair[0] == variable)
+        {
             return pair[1]; 
         } 
     } 
@@ -15,17 +35,25 @@ function hideOlder()
     var sourceVersion = getQueryVariable('version');
     if (!sourceVersion)
         return;
+    sourceVersion = sourceVersion.split('.');
 
     var hideFollowing = false;
     var versions = document.getElementsByClassName('version');
     for (var i = 0; i < versions.length; ++i)
     {
-        var version = versions[i];
-        if (version.id == sourceVersion)
-            hideFollowing = true;
-
         if (hideFollowing)
+        {
             version.style.display = 'none';
+        }
+        else
+        {
+            var version = versions[i].split('.');
+            if (versionGreaterThan(version, sourceVersion))
+            {
+                version.style.display = 'none';
+                hideFollowing = true;
+            }
+        }
     }
 }
 
